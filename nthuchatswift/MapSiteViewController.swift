5ad7d221080631831fd53f2767afbded786511fd
asdfghj1237890 <asdfghj1237890@yahoo.com.hk>
2018-11-21 05:35:22 +0800

    2018/11/21 2nd commit
    Implemented  showing activity by select the items in the TabBar

--- nthuchatswift/MapSiteViewController.swift
+++ nthuchatswift/MapSiteViewController.swift
@@ -6,12 +6,13 @@ import GoogleMaps
 import CoreLocation
 import Floaty
 
-class MapSiteViewController: UIViewController, GMSMapViewDelegate,UINavigationControllerDelegate, UITextViewDelegate, UISideMenuNavigationControllerDelegate,CLLocationManagerDelegate{
+class MapSiteViewController: UIViewController, GMSMapViewDelegate,UINavigationControllerDelegate, UITextViewDelegate, UISideMenuNavigationControllerDelegate,CLLocationManagerDelegate, UITabBarDelegate{
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var floaty: Floaty!
     
     var locationManager = CLLocationManager()
     var titleLabel: String!
+    @IBOutlet weak var tabbar: UITabBar!
     
     
     
@@ -74,6 +75,8 @@ class MapSiteViewController: UIViewController, GMSMapViewDelegate,UINavigationCo
         mapView.settings.myLocationButton = true
         //Location Manager code to fetch current location
         self.locationManager.startUpdatingLocation()
+        
+        tabbar.delegate = self
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -107,6 +110,92 @@ class MapSiteViewController: UIViewController, GMSMapViewDelegate,UINavigationCo
         //Finally stop updating location otherwise it will come again and again in this delegate
         self.locationManager.stopUpdatingLocation()
         
+    }
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem){
+        let selectedIndex = tabBar.items?.index(of: item)
+        let innovationref = Database.database().reference().child("activity").child("innovation")
+        let learningref = Database.database().reference().child("activity").child("learning")
+        let amuseref = Database.database().reference().child("activity").child("amusement")
+        let socialref = Database.database().reference().child("activity").child("social")
+        
+        if selectedIndex == 0 {
+            //print("啟發")
+            self.mapView.clear()
+            innovationref.observeSingleEvent(of: .value, with: { (snapshot) in
+                for child in snapshot.children{
+                    let eventDict = (child as? DataSnapshot)!.value as? [String: AnyObject] ?? [:]
+                    let title = (eventDict["title"] as! String)
+                    let location = (eventDict["location"])?.components(separatedBy: ",")
+                    let description = (eventDict["description"] as! String)
+                    //print("addevent:", title)
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: CLLocationDegrees(location![0])!, longitude: CLLocationDegrees(location![1])!)
+                    marker.map = self.mapView
+                    marker.title = title
+                    marker.snippet = description
+                    marker.icon = GMSMarker.markerImage(with: .red)
+                }
+            })
+        }else if selectedIndex == 1{
+            //print("學習")
+            self.mapView.clear()
+            learningref.observeSingleEvent(of: .value, with: { (snapshot) in
+                for child in snapshot.children{
+                    let eventDict = (child as? DataSnapshot)!.value as? [String: AnyObject] ?? [:]
+                    let title = (eventDict["title"] as! String)
+                    let location = (eventDict["location"])?.components(separatedBy: ",")
+                    let description = (eventDict["description"] as! String)
+                    //print("addevent:", title)
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: CLLocationDegrees(location![0])!, longitude: CLLocationDegrees(location![1])!)
+                    marker.map = self.mapView
+                    marker.title = title
+                    marker.snippet = description
+                    marker.icon = GMSMarker.markerImage(with: .red)
+                }
+            })
+        }else if selectedIndex == 2{
+            //print("娛樂")
+            self.mapView.clear()
+            amuseref.observeSingleEvent(of: .value, with: { (snapshot) in
+                for child in snapshot.children{
+                    let eventDict = (child as? DataSnapshot)!.value as? [String: AnyObject] ?? [:]
+                    let title = (eventDict["title"] as! String)
+                    let location = (eventDict["location"])?.components(separatedBy: ",")
+                    let description = (eventDict["description"] as! String)
+                    //print("addevent:", title)
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: CLLocationDegrees(location![0])!, longitude: CLLocationDegrees(location![1])!)
+                    marker.map = self.mapView
+                    marker.title = title
+                    marker.snippet = description
+                    marker.icon = GMSMarker.markerImage(with: .red)
+                }
+            })
+        }else if selectedIndex == 3{
+            //print("社交")
+            self.mapView.clear()
+            socialref.observeSingleEvent(of: .value, with: { (snapshot) in
+                for child in snapshot.children{
+                    let eventDict = (child as? DataSnapshot)!.value as? [String: AnyObject] ?? [:]
+                    let title = (eventDict["title"] as! String)
+                    let location = (eventDict["location"])?.components(separatedBy: ",")
+                    let description = (eventDict["description"] as! String)
+                    //print("addevent:", title)
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: CLLocationDegrees(location![0])!, longitude: CLLocationDegrees(location![1])!)
+                    marker.map = self.mapView
+                    marker.title = title
+                    marker.snippet = description
+                    marker.icon = GMSMarker.markerImage(with: .red)
+                }
+            })
+        }else if selectedIndex == 4{
+            //print("朋友")
+            self.mapView.clear()
+            
+        }
     }
     
     @IBAction func signOutScreen() {
